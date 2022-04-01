278a49735cdf1f75e9e47e239d0aaa2bc6d70999
MoellerO <oliver.moeller@die-olchies.de>
2022-04-01 12:57:34 +0200

    implement cost function and eval_classifier

--- slp.py
+++ slp.py
@@ -11,9 +11,10 @@ K = 10
 D = 3072
 MU = 0
 SIGMA = 0.01
+LAMBDA = 0.01
 
 
-def LoadBatch(filename):
+def load_batch(filename):
     """ Copied from the dataset website"""
     import pickle
     with open('Datasets/'+filename, 'rb') as fo:
@@ -64,25 +65,40 @@ def init_weights(mu, sigma):
 
 
 def init_bias(mu, sigma):
-    b = np.random.normal(mu, sigma, size=K)
-    return b
-
-# (X: d*n, W:K*d, b:K*1, p:K*n)
+    b = np.random.normal(mu, sigma, size=K)[np.newaxis]
+    return np.transpose(b)
 
 
-def Evaluate_Classifier(X, W, b):
+def evaluate_classifier(X, W, b):
+    # (X: d*n, W:K*d, b:K*1, p:K*n)
+    batch_size = X.shape[1]
+    ones_batch_size = np.ones(batch_size)[np.newaxis]
     # s = Wx+b
-    # p = SOFTMAX(s)
-    # SOFTMAX(s) = exp(s)/(1^T)exp(s)
-    P = 0
+    s = np.matmul(W, X) + np.matmul(b, ones_batch_size)
 
+    # P = SOFTMAX(s)
+    # SOFTMAX(s) = exp(s)/(1^T)exp(s)
+    # denominator = (1^T)exp(s)
+    denominator = np.matmul(np.ones(K), np.exp(s))
+    P = np.exp(s)/denominator
     return P
 
 
+def compute_cost(X, Y_one_hot, W, b, Lambda):
+    # (X: d*n, Y:k*n ,W:K*d, b:K*1, lamda: scalar)
+    D = X.shape[1]
+    P = evaluate_classifier(X, W, b)
+    Y_t = np.transpose(Y_one_hot)
+    l_cross = (-1) * np.matmul(Y_t, np.log(P))
+    # J =  1/D * Sum(l_cross(x,y,W,b)+ lambda * Sum(W_{i,j}^2))
+    J = 1/D * np.trace(l_cross) + Lambda * np.square(W).sum()
+    return J
+
+
 # load training, validation, and test data
-X_training, Y_training, y_training = LoadBatch(TRAINING_FILE)
-X_validation, Y_validation, y_validation = LoadBatch(VALIDATION_FILE)
-X_test, Y_test, y_test = LoadBatch(TEST_FILE)
+X_training, Y_training, y_training = load_batch(TRAINING_FILE)
+X_validation, Y_validation, y_validation = load_batch(VALIDATION_FILE)
+X_test, Y_test, y_test = load_batch(TEST_FILE)
 
 # show example images
 # montage(X_training)
@@ -100,9 +116,7 @@ X_norm_test = normalize(X_test, mean_training, std_training)
 # initialize weights and bias
 W = init_weights(MU, SIGMA)
 b = init_bias(MU, SIGMA)
-print(W.shape)
-print(b.shape)
 
-# norm_X = deprecated_normalize_X(X_training)
-# print(norm_X.shape)
-# print(norm_X)
+# Evaluate: softmax(WX+p)
+evaluate_classifier(X_norm_train, W, b)
+compute_cost(X_norm_train, Y_training, W, b, LAMBDA)
